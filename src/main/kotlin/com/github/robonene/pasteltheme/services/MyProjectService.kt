bb40164afb9504a07aa457ffcebb25179c2bf24c
GitHub Action <action@github.com>
2023-02-23 14:38:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/robonene/pasteltheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/robonene/pasteltheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.robonene.pasteltheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.robonene.pasteltheme.MyBundle
 
 class MyProjectService(project: Project) {
 
